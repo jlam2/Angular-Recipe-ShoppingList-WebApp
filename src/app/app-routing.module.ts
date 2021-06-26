1b6615f66a53e26188fa73c74d61a822d1b07482
jlam2 <jlam2@uncc.edu>
2021-06-26 13:34:17 -0400

    Section 22 - 327

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,9 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AuthComponent } from "./auth/auth.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
-import { AuthGuard } from "./auth/auth.guard";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipesEditComponent } from "./recipes/recipes-edit/recipes-edit.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children:[
-        {path: '', component: RecipeStartComponent},
-        {path: 'new', component: RecipesEditComponent},
-        {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
-        {path: ':id/edit', component: RecipesEditComponent, resolve: [RecipesResolverService]}
-    ] },
-    {path: 'shopping-list', component: ShoppingListComponent},
     {path: 'auth', component: AuthComponent}
 ];
 
